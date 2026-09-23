0bae379ed04add0d03c48fc73b828f33331b69b7
PouletDesMers <droucook@gmail.com>
2026-09-23 16:51:07 +0200

    add : test main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Utilisateur> utilisateurs = new ArrayList<>();
+    utilisateurs.add(new Etudiant((long) 1, "Jean", "Jean@gmail.com", "we231da", Niveau.L3));
+    utilisateurs.add(new Formateur((long) 2, "Pierre", "Pierre@gmail.com", (byte) 5));
+    for (Utilisateur u : utilisateurs) {
+    System.out.println(u.afficherProfil());
+}
     }
 }
